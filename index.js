fd1ac0ec52ed2cfafa801c152a7a83e52d066845
Alessandro Astone <ales.astone@gmail.com>
2019-03-17 12:39:36 +0100

    run eslint
     uh boy

--- index.js
+++ index.js
@@ -1,88 +1,89 @@
-const net = require('net');
-const fork = require('child_process').fork;
-const Commands = require('./utils/Commands.js').Commands;
-const OpCode = require('./utils/Commands.js').OpCode;
+const net = require("net")
+const fork = require("child_process").fork
+const Commands = require("./utils/Commands.js").Commands
+const OpCode = require("./utils/Commands.js").OpCode
 
-const MM_PORT = 50999;
-const BASE_PORT = 51000;
-const MAX_LOBBIES = 100;
-const MAX_PLAYER_COUNT = 2;
+const MM_PORT = 50999
+const BASE_PORT = 51000
+const MAX_LOBBIES = 100
+const MAX_PLAYER_COUNT = 2
 const forkOpts = {
-    stdio: [ 'pipe', 'pipe', 'pipe', 'ipc' ]
-};
+    stdio: [ "pipe", "pipe", "pipe", "ipc" ]
+}
 
-const lobbies = {}; // Object of lobbies, port: process
+const lobbies = {} // Object of lobbies, port: process
 
 const server = net.createServer(socket => {
-    console.log("New client connected");
-    socket.on("error", err => console.log(err));
-    socket.on('data', data => {
-        let view = new DataView(data.buffer);
-        const op = view.getUint8(0);
+    console.log("New client connected")
+    socket.on("error", err => console.log(err))
+    socket.on("data", data => {
+        let view = new DataView(data.buffer)
+        const op = view.getUint8(0)
         switch(op){
-            case OpCode.Queue:
+            case OpCode.Queue:{
                 // TODO: actual matchmaking
-                let lobby = Matchmake();
+                let lobby = Matchmake()
 
                 if(!lobbies[lobby]){
                     // Spawn a server for the lobby
-                    const lobbyServer = fork('lobby/lobby.js', [lobby], forkOpts);
-                    lobbyServer.stdout.on('data', data => console.log(`[${lobby}]: ${data}`)); // Log its console here as '[lobby]: output'
-                    lobbyServer.stderr.on('data', data => console.log(`[${lobby}]: ${data}`));
-                    lobbyServer.on('message', data => {
+                    const lobbyServer = fork("lobby/lobby.js", [lobby], forkOpts)
+                    lobbyServer.stdout.on("data", data => console.log(`[${lobby}]: ${data}`)) // Log its console here as '[lobby]: output'
+                    lobbyServer.stderr.on("data", data => console.log(`[${lobby}]: ${data}`))
+                    lobbyServer.on("message", data => {
                         switch(data.msg){
-                            case 'Online':
-                                lobbies[lobby].online = true;
-                                break;
-                            case 'PlayerCount':
-                                lobbies[lobby].playerCount = data.args[0];
+                            case "Online":
+                                lobbies[lobby].online = true
+                                break
+                            case "PlayerCount":
+                                lobbies[lobby].playerCount = data.args[0]
                         }
                     })
-                    lobbyServer.on('exit', code => {
+                    lobbyServer.on("exit", () => {
                         // Remove it from the array
-                        delete lobbies[lobby];
-                        console.log(`Killed server ${lobby}`);
+                        delete lobbies[lobby]
+                        console.log(`Killed server ${lobby}`)
                     })
                     // Defaults
-                    lobbyServer.online = false;
-                    lobbyServer.playerCount = 0;
+                    lobbyServer.online = false
+                    lobbyServer.playerCount = 0
 
-                    lobbies[lobby] = lobbyServer;
+                    lobbies[lobby] = lobbyServer
                 }
 
-                let watch = setInterval(()=>{
-                    if(!lobbies[lobby] || !lobbies[lobby].online) return;
-                    let matchCmd = new Commands.FoundMatch(lobby);
-                    socket.write(matchCmd.Buffer);
+                let watch = setInterval( () => {
+                    if(!lobbies[lobby] || !lobbies[lobby].online) return
+                    let matchCmd = new Commands.FoundMatch(lobby)
+                    socket.write(matchCmd.Buffer)
 
-                    clearInterval(watch);
-                }, 20);
+                    clearInterval(watch)
+                }, 20)
 
-                break;
+                break
+            }
         }
-    });
+    })
 })
-.listen(MM_PORT, () => console.log(`master server listening on port ${server.address().port}`));
+    .listen(MM_PORT, () => console.log(`master server listening on port ${server.address().port}`))
 
 function Matchmake(){
-    let freePorts = [];
-    let lobbyPorts = Object.keys(lobbies);
-    for(let i=0; i<lobbyPorts.length; i++){
-        let _lobby = lobbies[lobbyPorts[i]];
+    let freePorts = []
+    let lobbyPorts = Object.keys(lobbies)
+    for(let i = 0; i < lobbyPorts.length; i++){
+        let _lobby = lobbies[lobbyPorts[i]]
         if(_lobby.online && _lobby.playerCount < MAX_PLAYER_COUNT)
-            freePorts.push(lobbyPorts[i]);
+            freePorts.push(lobbyPorts[i])
     }
 
-    let ret;
+    let ret
     if(freePorts.length > 0){
-        ret = freePorts[Math.floor(Math.random() * freePorts.length)];
+        ret = freePorts[Math.floor(Math.random() * freePorts.length)]
     }
     else{
         // New random port
         do {
-            ret = BASE_PORT + Math.floor(Math.random() * MAX_LOBBIES);
-        } while(lobbies[ret] != undefined);
+            ret = BASE_PORT + Math.floor(Math.random() * MAX_LOBBIES)
+        } while(lobbies[ret] != undefined)
     }
 
-    return ret;
+    return ret
 }
--- utils/NetCode.js
+++ utils/NetCode.js
@@ -2,13 +2,13 @@ class NetCode {
 
     /**
      * Send command to a Player
-     * @param {Command} command 
-     * @param {socket} from 
-     * @param {socket} to 
+     * @param {Command} command
+     * @param {socket} from
+     * @param {socket} to
      */
     static Send(command, from, to){
-        from.send(command.Buffer, to.port, to.address);
+        from.send(command.Buffer, to.port, to.address)
     }
 }
 
-module.exports = NetCode;
+module.exports = NetCode
